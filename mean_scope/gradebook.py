b3d47cce419fe5ec072bea3fae12af2b338b1285
matt higger <matt.higger@gmail.com>
2022-05-04 16:54:26 -0400

    revise - gradebook - now stores metadata in its own dataframe (df_meta)
    (cherry picked from commit 8d95b5f22ec2705f524d68ce857d2838ab5be429)

--- mean_scope/gradebook.py
+++ mean_scope/gradebook.py
@@ -1,41 +1,48 @@
+from copy import copy
 from math import ceil
 
 import numpy as np
 import pandas as pd
 
 from .assign_list import AssignmentList
+from .get_mean_drop_low import get_mean_drop_low
 
 
 class Gradebook:
     """ a grade for every student-assignment pair & manipulations
 
     Attributes:
-        df (pd.DataFrame): index is email of student and each col is assignment
-            values are percentage student earned (nan for waived).  contains
-            a few metadata columns too (first name, last name, section, id)
-
+        df_perc (pd.DataFrame): index is email of student and each col is assignment
+            values are percentage student earned (nan for waived).
+        df_meta (pd.DataFrame): index is email, columns are metadata (first
+            name, last name, section, id)
         df_lateday (pd.DataFarme): index are email, cols are assignment and
             values are days each assignment is late
         ass_list (AssignmentList): a list of assignments
         points (np.array): points per assignment (same order as ass_list)
     """
+    META_DATA_COLS = 4
 
     def __init__(self, f_scope):
         df_scope = pd.read_csv(f_scope, index_col='Email')
 
-        # lowercase input data
-        meta_data_cols = 4
-        for idx in range(meta_data_cols):
-            df_scope.iloc[:, idx] = \
-                df_scope.iloc[:, idx].map(lambda x: str(x).lower())
+        # groom input data
+        df_scope.columns = [s.lower() for s in df_scope.columns]
+        for idx in range(self.META_DATA_COLS):
+            if df_scope.columns[idx] == 'sid':
+                # student ids are ints, lets not cast to str
+                continue
+            df_scope.iloc[:, idx] = df_scope.iloc[:, idx].map(str.lower)
         df_scope.index.map(str.lower)
         df_scope.index.name = df_scope.index.name.lower()
-        df_scope.columns = [s.lower() for s in df_scope.columns]
         df_scope.fillna(0, inplace=True)
+
+        # store meta data
+        self.df_meta = df_scope.iloc[:, :self.META_DATA_COLS]
 
         # compute percent per assignment & points
         self.ass_list = AssignmentList(df_scope.columns)
-        self.df = pd.DataFrame()
+        self.df_perc = pd.DataFrame()
         self.points = np.empty(len(self.ass_list))
         for idx, ass in enumerate(self.ass_list):
             # points per assignment
@@ -45,7 +52,7 @@ class Gradebook:
             self.points[idx] = df_scope[ass_max_pt].values[0]
 
             # percentage per assignment
-            self.df[ass] = df_scope[ass] / df_scope[ass_max_pt]
+            self.df_perc[ass] = df_scope[ass] / df_scope[ass_max_pt]
 
         # compute days late
         def get_days_late(s_hour_min_sec):
@@ -67,7 +74,7 @@ class Gradebook:
 
         for email, ass_many in waive_dict.items():
             for ass in ass_many.split(','):
-                self.df.loc[email, self.ass_list.normalize(ass)] = np.nan
+                self.df_perc.loc[email, self.ass_list.normalize(ass)] = np.nan
 
     def substitute(self, sub_dict):
         """ substitutes some assignment percentages (if sub is higher)
@@ -90,11 +97,12 @@ class Gradebook:
                 ass_from_list = ass_from_list + [ass_to]
 
             # get max percentage across all assignments, substitute it
-            new_col_dict[ass_to] = self.df.loc[:, ass_from_list].max(axis=1)
+            new_col_dict[ass_to] = self.df_perc.loc[:, ass_from_list].max(
+                axis=1)
 
         # substitute
         for ass_to, s in new_col_dict.items():
-            self.df[ass_to] = s
+            self.df_perc[ass_to] = s
 
     def remove(self, ass):
         """ deletes an assignment
@@ -107,7 +115,46 @@ class Gradebook:
         ass_idx = self.ass_list.index(ass)
 
         # remove
-        del self.df[ass]
+        del self.df_perc[ass]
         del self.df_lateday[ass]
         self.ass_list.pop(ass_idx)
         self.points = np.delete(self.points, ass_idx)
+
+    def average(self, cat_weight_dict=None):
+        """ final grades, weighted by points (default) or category weights
+
+        Args:
+            cat_weight_dict (dict): keys are strings which define categories
+                values are unnormalized (positive) weights assigned to each
+                category.  (e.g. if HW / exam each worth 50% of grade then
+                cat_weight_dict {'hw': 50, 'exam': 50}.  Assignment names must
+                each contain exactly one category such that categories
+                partition all assignments.  (Default: no categories given, each
+                assignment weighted by points given on gradescope)
+
+        Returns:
+            df_grade (pd.DataFrame): contains self.df_perc with added columns of
+                weighted mean (and mean of any category)
+        """
+        if cat_weight_dict is None:
+            # all assignments contain ''
+            cat_weight_dict = {'': 1}
+
+        # extract percentages as array (a bit quicker)
+        perc_all = self.df_perc.iloc[:, self.META_DATA_COLS:].values
+
+        df_grade = copy(self.df_perc)
+        for cat, weight in cat_weight_dict.items():
+            # boolean index into assignments of given category
+            cat_bool = np.array([cat in ass for ass in self.ass_list])
+            perc_cat = perc_all[:, cat_bool]
+            weight_cat = self.points_cat[:, cat_bool]
+
+            for idx, email in enumerate(self.df_perc.index):
+                # compute mean per student-category
+                s_mean = f'mean_{cat}'
+                _perc = perc_cat[idx, :]
+                _weight = weight_cat[idx, :]
+                df_grade.loc[email, s_mean] = get_mean_drop_low(_perc, _weight)
+
+        return df_grade
--- test/test_gradebook.py
+++ test/test_gradebook.py
@@ -10,9 +10,11 @@ def gradebook():
 
 class TestGradebook:
     def test_init(self, gradebook):
-        df_exp = pd.read_csv('df_exp.csv', index_col='email')
+        df_perc_exp = pd.read_csv('df_perc_exp.csv', index_col='email')
+        df_meta_exp = pd.read_csv('df_meta_exp.csv', index_col='email')
 
-        pd.testing.assert_frame_equal(df_exp, gradebook.df)
+        pd.testing.assert_frame_equal(df_meta_exp, gradebook.df_meta)
+        pd.testing.assert_frame_equal(df_perc_exp, gradebook.df_perc)
         assert np.allclose([1, 2, 3, 4], gradebook.points)
         assert np.allclose([2, 3, 0, 0, 0], gradebook.df_lateday.loc[:, 'hw1'])
 
@@ -21,23 +23,23 @@ class TestGradebook:
                       'last1@nu.edu': 'hw1, hw2'}
         gradebook.waive(waive_dict)
 
-        assert np.isnan(gradebook.df.loc['last0@nu.edu', 'hw1'])
-        assert np.isnan(gradebook.df.loc['last1@nu.edu', 'hw1'])
-        assert np.isnan(gradebook.df.loc['last1@nu.edu', 'hw2'])
+        assert np.isnan(gradebook.df_perc.loc['last0@nu.edu', 'hw1'])
+        assert np.isnan(gradebook.df_perc.loc['last1@nu.edu', 'hw1'])
+        assert np.isnan(gradebook.df_perc.loc['last1@nu.edu', 'hw2'])
 
     def test_substitute(self, gradebook):
         sub_dict = {'hw2': ['hw3'],
                     'hw1': ['hw1', 'hw2']}
         gradebook.substitute(sub_dict)
 
-        assert np.allclose([1, 1, 1, 1, 1], gradebook.df['hw2'])
-        assert np.allclose([1, 0, 0, .5, .5], gradebook.df['hw1'])
+        assert np.allclose([1, 1, 1, 1, 1], gradebook.df_perc['hw2'])
+        assert np.allclose([1, 0, 0, .5, .5], gradebook.df_perc['hw1'])
 
     def test_remove(self, gradebook):
         ass = 'hw1'
         gradebook.remove(ass)
 
-        assert ass not in gradebook.df.columns
+        assert ass not in gradebook.df_perc.columns
         assert ass not in gradebook.df_lateday.columns
         assert ass not in gradebook.ass_list
         assert np.allclose([2, 3, 4], gradebook.points)
