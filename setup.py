1f1b70720cc4c9cfa1501db148e0a7a8e276bf57
matt higger <matt.higger@gmail.com>
2022-05-04 14:42:44 -0400

    init commit
    (cherry picked from commit c322c94ba86e971f26dc3ef26faa0225aa1493bc)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(name='mean_scope',
+      description='utility to average gradescope csv output to final grades',
+      author='Matt Higger',
+      author_email='matt.higger@gmail.com',
+      packages=find_packages(),
+      install_requires=[
+          'numpy',
+          'pandas',
+          'pytest']
+      )
