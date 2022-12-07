5ef4b4e1d148e0da2b0b317c3e908ebc31a527d9
matt higger <matt.higger@gmail.com>
2022-12-07 16:17:08 -0500

    revise - perc_to_letter - support for non "sorted" dictionaries.  (dicts iterated in order of creation ... user inputs may not be created in decreasing threshold order)

--- gradescope_mean/perc_to_letter.py
+++ gradescope_mean/perc_to_letter.py
@@ -1,42 +1,47 @@
 import numpy as np
 
-GRADE_THRESH = {'A': .93,
-                'A-': .90,
-                'B+': .87,
-                'B': .83,
-                'B-': .80,
-                'C+': .77,
-                'C': .73,
-                'C-': .70,
-                'D+': .67,
-                'D': .63,
-                'D-': .60,
-                'E': 0}
+GRADE_THRESH = {.93: 'A',
+                .90: 'A-',
+                .87: 'B+',
+                .83: 'B',
+                .80: 'B-',
+                .77: 'C+',
+                .73: 'C',
+                .70: 'C-',
+                .67: 'D+',
+                .63: 'D',
+                .60: 'D-',
+                .00: 'E'}
 
 
-def perc_to_letter(perc, grade_thresh=None, float_bonus=1e-8):
+def perc_to_letter(perc, grade_thresh=None, float_bonus=1e-8,
+                   nan_letter='no-grade'):
     """ converts a percentage to a letter grade
 
     Args:
         perc (float): percentage (on assignment)
-        grade_thresh (dict): keys are letter grades, values are lower
-            thresholds (inclusive) to get grade
+        grade_thresh (dict): keys are lower thresholds (inclusive) to get
+            grade, values are letter grades
         float_bonus (float): due to floating point rounding errors, we give all
             students a slight bonus to ensure they don't miss out on any
             thresholds their grades meet their floating point average may just
             fall short of
+        nan_letter (str): assigned to nan inputs
 
     Returns:
         grade (str): letter grade
     """
     if grade_thresh is None:
-        # default
-        grade_thresh = GRADE_THRESH
+        # default (guaranteed sorted)
+        mark_thresh_iter = iter(GRADE_THRESH.items())
+    else:
+        # user input (we must sort)
+        mark_thresh_iter = sorted(grade_thresh.items(), reverse=True)
 
     if np.isnan(perc):
-        return 'no-grade'
+        return nan_letter
 
-    for mark, thresh in grade_thresh.items():
+    for thresh, mark in mark_thresh_iter:
         if perc + float_bonus >= thresh:
             return mark
 
