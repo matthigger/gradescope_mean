a320b0ffc2e8ca4928894c7609e78224a4fc0a81
matt higger <matt.higger@gmail.com>
2025-02-24 16:32:23 -0500

    add - #25 PCA graph for getting a sense of student spreads ... not really so easily readable just yet.  also, not complete as we need to account for assignment weight in this graph (it isn't right to interpret it without this!), graph could be a little nicer looking too

--- gradescope_mean/plot.py
+++ gradescope_mean/plot.py
@@ -1,3 +1,5 @@
+import numpy as np
+import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
@@ -34,5 +36,53 @@ def plot_hist(df_grade_full, cat_weight_dict):
         fig.add_hline(y=mean, annotation_text=f'mean: {mean:.3f}',
                       col=col_idx + 1, row=1)
     fig.update_layout(showlegend=False)
+
+    return fig
+
+
+def plot_pca(df_grade_full, cat_weight_dict):
+    """ plots scatter of students on 1st 2 PCA directions
+
+    (helps identify trends between assignments and spot outliers, likely hw
+    and exam scores are correlated, but maybe some sub-group have abnormally
+    high hw
+
+    Args:
+        df_grade_full (pd.DataFrame):
+        cat_weight_dict (dict): keys are categories, values are weights
+
+    Returns:
+        fig (plotly):
+    """
+    if cat_weight_dict is not None:
+        print('warning: category weight not accounted for, each assignment '
+              'given equal weight')
+
+    # kludge: assignments are all columns after 'letter'
+    idx_letter = list(df_grade_full.columns).index('letter')
+    df = df_grade_full.iloc[:, idx_letter + 1:].copy()
+
+    # center and impute
+    df -= df.mean()
+    df = df.fillna(0)
+
+    # pca projection
+    x = df.values
+    c = np.cov(x.T)
+    vals, vecs = np.linalg.eig(c)
+
+    # ensure pca vectors largely point "positive" (top right is for strong
+    # students in resulting graph)
+    flip = (vecs > 0).sum(axis=0) > x.shape[1] / 2
+    vecs = vecs @ np.diag(flip)
+
+    # prep p
+    df = df_grade_full.copy()
+    df['pca0'] = x @ vecs[:, 0]
+    df['pca1'] = x @ vecs[:, 1]
+    fig = px.scatter(df,
+                     x='pca0',
+                     y='pca1',
+                     hover_data=df.columns)
 
     return fig
